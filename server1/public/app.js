5bf5fc50ab237a3ea6f48010eac00522327ace47
Levon Hakopyan <levon.hakopyan@gmail.com>
2014-02-07 18:58:32 +0400

    rewrite code

--- server1/public/app.js
+++ server1/public/app.js
@@ -1,33 +1,22 @@
-var peer = new Peer({key: "oftz4qdmchjxxbt9"});
-var myId = undefined;
-
-peer.on('open', function(id) {
-  console.log('My peer ID is: ' + id);
-  myId = id;
-});
-
-// start connection
-var connect = function(destId) {
-	var conn = peer.connect(destId);
+var app = {
 	
-	conn.on("open", function() {
-		conn.send("hello from main page");
-	});
-};
+	writeId: function(id) {
+		document.getElementById("iframeId").innerHTML = id;
+	},
+	
+	connect: function(destId) {
+		this.writeId(destId);
+			
+		var peer = new Peer({key: "oftz4qdmchjxxbt9"});
+		var conn = peer.connect(destId);
 
-// recive connection
-peer.on('connection', function(conn) {
-	conn.on('data', function(data) {
-	  alert(data);
-	});
-});
-
-var sendId = function() {
-	var win = document.getElementById("iframe").contentWindow;
-	win.postMessage(myId, "http://localhost:8002");
+		conn.on("open", function() {
+			conn.send("CONNECTED");
+		});
+	}
+	
 };
 
 window.addEventListener("message", function(event) {
-	document.getElementById("iframeId").innerHTML = event.data;
-	connect(event.data);
+	app.connect(event.data);
 }, false);
--- server2/public/app.js
+++ server2/public/app.js
@@ -1,34 +1,23 @@
-//frame script
-
-var peer = new Peer({key: "oftz4qdmchjxxbt9"});
-var myId = undefined;
-
-peer.on('open', function(id) {
-  console.log('iframe peer ID is: ' + id);
-  myId = id;
-});
-
-//start connection
-var connect = function(destId) {
-	var conn = peer.connect(destId);
-
-	conn.on("open", function() {
-		conn.send("hello from iframe");
-	});
-};
+var app = {
+	myId: undefined,
 	
-//recive coonection
-peer.on('connection', function(conn) {
-	conn.on('data', function(data) {
-		alert(data);
-	});
-});
+	sendId: function(id) {
+		window.parent.postMessage(id, "http://localhost:8001");
+	},
+	
+	connect: function() {
+		var peer = new Peer({key: "oftz4qdmchjxxbt9"});
+		
+		peer.on('open', function(id) {
+			app.sendId(id);
+		});
 
-var sendId = function() {
-	window.parent.postMessage(myId, "http://localhost:8001");
+		peer.on('connection', function(conn) {
+			conn.on('data', function(data) {
+				document.getElementById("status").innerHTML = data;
+			});
+		});
+
+	}
+	
 };
-
-window.addEventListener("message", function(event) {
-	document.getElementById("mainId").innerHTML = event.data;
-	connect(event.data);
-}, false);
